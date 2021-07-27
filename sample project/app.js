eb991ce817d0d41003fe81d233ebfd1d5fe74253
SteppenHasen <dustasura@gmail.com>
2021-07-27 21:57:38 +0300

    Make async GET request with axios

--- sample project/app.js
+++ sample project/app.js
@@ -1,2 +1,37 @@
-const BREEDS_URL = 'https://dog.ceo/api/breeds/list/all';
+(async () => {
 
+    const axios = require('axios').default;
+
+    async function getBreedsList() {
+        return axios.get('https://dog.ceo/api/breeds/list/all').then((resp) => {
+            return resp.data;
+        });
+    }
+    async function getBreedRandom() {
+        return axios.get('https://dog.ceo/api/breeds/image/random').then((resp) => {
+            return resp.data;
+        });
+    }
+    async function getBreedImgs() {
+        return axios.get('https://dog.ceo/api/breed/hound/images').then((resp) => {
+            return resp.data;
+        });
+    }
+
+    async function getSubBreedsList() {
+        return axios.get('https://dog.ceo/api/breed/hound/list').then((resp) => {
+            return resp.data;
+        });
+    }
+
+    let breedsListJSON = await getBreedsList()
+    let breedRandomJSON = await getBreedRandom()
+    let breedImgsJSON = await getBreedImgs()
+    let subBreedsListJSON = await getSubBreedsList()
+
+    console.log(breedsListJSON)
+    console.log(breedRandomJSON)
+    console.log(breedImgsJSON)
+    console.log(subBreedsListJSON)
+
+})();

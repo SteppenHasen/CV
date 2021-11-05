e75115c9e0cb263d3b25dbde71ab26900e965c9e
SteppenHasen <dustasura@gmail.com>
2021-11-05 22:37:21 +0300

    Simple animation

--- script.js
+++ script.js
@@ -1,8 +1,58 @@
+var isScrolling = false;
+
 function showHideMenu() {
-    let links = document.getElementById("navlinks");
-    if (links.style.display === "block") {
-      links.style.display = "none";
+  let links = document.getElementById("navlinks");
+  if (links.style.display === "block") {
+    links.style.display = "none";
+  } else {
+    links.style.display = "block";
+  }
+}
+
+window.addEventListener("scroll", throttleScroll, false);
+
+function throttleScroll(e) {
+  if (isScrolling == false) {
+    window.requestAnimationFrame(function() {
+      scrolling(e);
+      isScrolling = false;
+    });
+  }
+  isScrolling = true;
+}
+
+function isPartiallyVisible(el) {
+  var elementBoundary = el.getBoundingClientRect();
+
+  var top = elementBoundary.top;
+  var bottom = elementBoundary.bottom;
+  var height = elementBoundary.height;
+
+  return ((top + height >= 0) && (height + window.innerHeight >= bottom));
+}
+
+function isFullyVisible(el) {
+  var elementBoundary = el.getBoundingClientRect();
+ 
+  var top = elementBoundary.top;
+  var bottom = elementBoundary.bottom;
+ 
+  return ((top >= 0) && (bottom <= window.innerHeight));
+}
+
+document.addEventListener("DOMContentLoaded", scrolling, false);
+ 
+var listItems = document.querySelectorAll("#animate");
+
+function scrolling(e) {
+
+  for (var i = 0; i < listItems.length; i++) {
+    var listItem = listItems[i];
+
+    if (isPartiallyVisible(listItem)) {
+      listItem.classList.add("active");
     } else {
-      links.style.display = "block";
+      listItem.classList.remove("active");
     }
   }
+}
